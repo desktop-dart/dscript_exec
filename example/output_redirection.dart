d26b71c26974e3d6cb034b3db655924ad5734056
Brett Morgan <brett.morgan@gmail.com>
2020-02-07 19:23:49 -0800

    Fix output_redirection.dart as well

--- example/output_redirection.dart
+++ example/output_redirection.dart
@@ -5,7 +5,7 @@ import 'package:dscript_exec/dscript_exec.dart';
 
 Future<void> main() async {
   await (exec('cat', ['example/names.csv']) |
-              exec('cut', ['-d', "','", '-f', '1']) >
+              exec('cut', ['-d', ',', '-f', '1']) >
           Uri(path: 'example/dest.csv'))
       .run();
 }
